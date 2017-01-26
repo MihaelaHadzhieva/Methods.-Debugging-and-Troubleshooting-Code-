6948591236f9803be5086dbb49af87aa06c735c8
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-27 01:39:59 +0200

    Add files via upload

---
+++ FactorialTrailingZeroes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialTrailingZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Parse(Console.ReadLine());
+            Factorial(number);
+        }
+        public static void Factorial(BigInteger number)
+        {
+            BigInteger result = 1;
+            for (BigInteger i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+
+            long timesZero = 0;
+
+            while (result % 10 == 0)
+            {
+                timesZero++;
+                result /= 10;
+            }
+            Console.WriteLine(timesZero);
+        }
+
+    }
+}
