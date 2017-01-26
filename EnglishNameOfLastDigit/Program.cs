439c20504998770507cc95606ff140c1f42d30b5
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-27 01:28:24 +0200

    Add files via upload

---
+++ EnglishNameOfLastDigit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishNameOfLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            EglishNameOftheDigit(Math.Abs(number));
+
+        }
+        public static void EglishNameOftheDigit(long num)
+        {
+            long lastDigit = 0;
+            lastDigit = num % 10;
+            switch (lastDigit)
+            {
+                case 1: Console.WriteLine("one"); break;
+                case 2: Console.WriteLine("two"); break;
+                case 3: Console.WriteLine("three"); break;
+                case 4: Console.WriteLine("four"); break;
+                case 5: Console.WriteLine("five"); break;
+                case 6: Console.WriteLine("six"); break;
+                case 7: Console.WriteLine("seven"); break;
+                case 8: Console.WriteLine("eight"); break;
+                case 9: Console.WriteLine("nine"); break;
+                case 0: Console.WriteLine("zero"); break;
+            }
+        }
+    }
+}
