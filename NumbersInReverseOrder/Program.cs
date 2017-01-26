fad2e778408da09031021864a6863f0b598e4a6b
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-27 01:29:18 +0200

    Add files via upload

---
+++ NumbersInReverseOrder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            Console.WriteLine(PrintInRevereseOrder(number));
+
+        }
+        public static string PrintInRevereseOrder(string number)
+        {
+            char[] reverseNumber = number.ToCharArray();           
+            Array.Reverse(reverseNumber);
+            return new string(reverseNumber);
+        }
+
+    }
+}
