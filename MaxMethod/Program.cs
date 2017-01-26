1a32c412b97fe5ea0291d1d740e25fefe7dbe969
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-27 01:27:06 +0200

    Add files via upload

---
+++ MaxMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMax(firstNum, secondNum, thirdNum)); 
+        }
+        public static int GetMax(int firstNum, int secondNum, int thirdNum)
+        {
+            int Maxnumber = Math.Max(firstNum, secondNum);
+            return Math.Max(Maxnumber, thirdNum);       
+        }
+    }
+}
