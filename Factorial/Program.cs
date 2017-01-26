98f9a1f6e06bf3af6cad775fa26546cdebf0f44d
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-27 01:39:04 +0200

    Add files via upload

---
+++ Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Parse(Console.ReadLine());
+            Factorial(number);
+        }
+        public static void Factorial(BigInteger number)
+        {
+            BigInteger result = 1;
+            for (BigInteger i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
