a2d1e19dd18e707fb9b1d3483e5146df549496f0
MihaelaHadzhieva <mihaela.h.99@abv.bg>
2017-01-27 01:30:45 +0200

    Add files via upload

---
+++ PrimeChecker/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            Console.WriteLine(IsPrime(number)); 
+        }
+        public static bool IsPrime(long number)
+        {
+            bool result = true;
+            if (number==0 || number==1)
+            {
+                return false;
+            }
+            for (int dividend = 2; dividend <= Math.Sqrt(number); dividend++)
+            {                  
+                   if (number % dividend == 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
+
